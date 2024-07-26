7da741436afde93e1c6ca00ffb96d3425e85bb1a
mahmoud <mahmodeljaru@gmail.com>
2024-07-26 16:33:49 +0300

    absolute search engine

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'DRAFT'
@@ -18,10 +18,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
+    
+    # مهمة لمحركات البحث 
+    def get_absolute_url(self):
+            return reverse("blog:post-detail", args=[self.slug])
+        
     class Meta:
         ordering = ['-pub_date']
         indexes = [
             models.Index(fields=['-pub_date']),
             models.Index(fields=['slug']),
         ]
+
+
