dc76acc63a769ac8fc6910c70db2f3c6ed018f43
mahmoud <mahmodeljaru@gmail.com>
2024-07-27 19:49:56 +0300

    add taggit

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+
+from taggit.managers import TaggableManager
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'DRAFT'
@@ -15,6 +17,8 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    tag = TaggableManager()
 
     def __str__(self):
         return self.title
--- post_project/settings.py
+++ post_project/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     "crispy_forms",
     "crispy_bootstrap5",
+    'taggit',
 ]
 
 MIDDLEWARE = [
